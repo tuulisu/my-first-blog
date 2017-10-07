62b0c6d8402298deab8738f7a639df928c0994e6
Tuuli <tuuli.suomela@live.fi>
2017-10-07 12:25:43 +0300

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+print('Hello World')
+name = "Tuuli"
+adress = "Kirkkoherrantie 4 a 2 00650 Helsinki"
+phone = "0445357031"
+print("name" + "adress" + "phone")
+print(name + adress + phone)
+print(name_ + adress_ + phone_)
